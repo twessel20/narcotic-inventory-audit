fcaa750859d82bc4b400134446f8c9c628eb6419
twessel20 <twessel20@aol.com>
2026-09-25 18:51:37 -0500

    Serve improved PDF section pagination

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='narcotic-audit-v82';
-const ASSETS=['./','./index.html','./styles.css?v=20260925-82','./app.js?v=20260925-82','./manifest.webmanifest'];
+const CACHE='narcotic-audit-v83';
+const ASSETS=['./','./index.html','./styles.css?v=20260925-83','./app.js?v=20260925-83','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
