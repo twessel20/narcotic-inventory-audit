d73ffd72e45b9897bede910c862aef949e5e0cda
twessel20 <twessel20@aol.com>
2026-09-25 19:55:04 -0500

    Serve Safe inventory explanation

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='narcotic-audit-v108';
-const ASSETS=['./','./index.html','./styles.css?v=20260925-108','./app.js?v=20260925-108','./manifest.webmanifest'];
+const CACHE='narcotic-audit-v109';
+const ASSETS=['./','./index.html','./styles.css?v=20260925-109','./app.js?v=20260925-109','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
