bd0d99b37d8b2a6e339286529a75d9c5a7fb1dce
twessel20 <twessel20@aol.com>
2026-09-25 22:31:34 -0500

    Bump cache for Safe witness pagination fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='narcotic-audit-v179';
-const ASSETS=['./','./index.html','./styles.css?v=20260925-179','./app.js?v=20260925-179','./manifest.webmanifest'];
+const CACHE='narcotic-audit-v180';
+const ASSETS=['./','./index.html','./styles.css?v=20260925-180','./app.js?v=20260925-180','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
